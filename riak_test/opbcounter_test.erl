5e50cf775756f9c5f58db5768b89006ade53685c
balegas <v.sousa@campus.fct.unl.pt>
2015-10-16 13:56:45 +0100

    riak_tests bounded counter WIP

--- riak_test/opbcounter_test.erl
+++ riak_test/opbcounter_test.erl
@@ -35,7 +35,6 @@ confirm() ->
     new_bcounter_test(Nodes),
     increment_test(Nodes),
     decrement_test(Nodes),
-    transfer_test(Nodes),
     conditional_write_test(Nodes),
     %rt:clean_cluster(Nodes),
     pass.
@@ -55,26 +54,28 @@ new_bcounter_test(Nodes) ->
 %% Tests incrementing a `bcounter()'.
 increment_test(Nodes) ->
     FirstNode = hd(Nodes),
+    SecondNode = hd(tl(Nodes)),
     lager:info("increment_test started"),
     Type = crdt_bcounter,
     Key = bcounter2,
     %% Test simple read and write operations.
     Result0 = rpc:call(FirstNode, antidote, append,
-                       [Key, Type, {{increment, 10}, r1}]),
+                       [Key, Type, {{increment, 10}, a}]),
     ?assertMatch({ok, _}, Result0),
     Result1 = rpc:call(FirstNode, antidote, read, [Key, Type]),
     {ok, Counter1} = Result1,
     ?assertEqual(10, crdt_bcounter:permissions(Counter1)),
-    ?assertEqual(10, crdt_bcounter:localPermissions(r1,Counter1)),
-    ?assertEqual(0, crdt_bcounter:localPermissions(r2,Counter1)),
+
+    %Cannot test local permissions on client-side, because it does not know the id of the server.
+    %?assertEqual(10, crdt_bcounter:localPermissions(r1,Counter1)),
+    %?assertEqual(0, crdt_bcounter:localPermissions(r2,Counter1)),
+
     %% Test bulk transaction with read and write operations.
-    Result2 = rpc:call(FirstNode, antidote, clocksi_execute_tx,
-                       [[{update, {Key, Type, {{increment, 7}, r1}}}, {update, {Key, Type, {{increment, 5}, r2}}}, {read, {Key, Type}}]]),
+    Result2 = rpc:call(SecondNode, antidote, clocksi_execute_tx,
+                       [[{update, {Key, Type, {{increment, 7}, b}}}, {update, {Key, Type, {{increment, 5}, b}}}, {read, {Key, Type}}]]),
     ?assertMatch({ok, _}, Result2),
     {ok, {_, [Counter2], _}} = Result2,
-    ?assertEqual(22, crdt_bcounter:permissions(Counter2)),
-    ?assertEqual(17, crdt_bcounter:localPermissions(r1,Counter2)),
-    ?assertEqual(5, crdt_bcounter:localPermissions(r2,Counter2)).
+    ?assertEqual(22, crdt_bcounter:permissions(Counter2)).
 
 %% Tests decrementing a `bcounter()'.
 decrement_test(Nodes) ->
@@ -84,47 +85,16 @@ decrement_test(Nodes) ->
     Key = bcounter3,
     %% Test an allowed chain of operations.
     Result0 = rpc:call(FirstNode, antidote, clocksi_execute_tx,
-                       [[{update, {Key, Type, {{increment, 7}, r1}}}, {update, {Key, Type, {{increment, 5}, r2}}},
-                         {update, {Key, Type, {{decrement, 3}, r2}}}, {update, {Key, Type, {{decrement, 2}, r1}}},
+                       [[{update, {Key, Type, {{increment, 7}, a}}}, {update, {Key, Type, {{increment, 5}, a}}},
+                         {update, {Key, Type, {{decrement, 3}, a}}}, {update, {Key, Type, {{decrement, 2}, a}}},
                          {read, {Key, Type}}]]),
     ?assertMatch({ok, _}, Result0),
     {ok, {_, [Counter0], _}} = Result0,
     ?assertEqual(7, crdt_bcounter:permissions(Counter0)),
-    ?assertEqual(5, crdt_bcounter:localPermissions(r1,Counter0)),
-    ?assertEqual(2, crdt_bcounter:localPermissions(r2,Counter0)),
     %% Test a forbidden chain of operations.
     Result1 = rpc:call(FirstNode, antidote, clocksi_execute_tx,
-                       [[{update, {Key, Type, {{decrement, 3}, r2}}}, {read, {Key, Type}}]]),
-    ?assertEqual({error, no_permissions}, Result1).
-
-%% Tests transferring permissions between replicas in a `bcounter()'.
-transfer_test(Nodes) ->
-    FirstNode = hd(Nodes),
-    lager:info("transfer_test started"),
-    Type = crdt_bcounter,
-    Key = bcounter4,
-    %% Initialize the `bcounter()' with some increment and decrement operations.
-    Result0 = rpc:call(FirstNode, antidote, clocksi_execute_tx,
-                       [[{update, {Key, Type, {{increment, 7}, r1}}}, {update, {Key, Type, {{increment, 5}, r2}}},
-                         {update, {Key, Type, {{decrement, 3}, r2}}}, {read, {Key, Type}}]]),
-    ?assertMatch({ok, _}, Result0),
-    {ok, {_, [Counter0], _}} = Result0,
-    ?assertEqual(9, crdt_bcounter:permissions(Counter0)),
-    ?assertEqual(7, crdt_bcounter:localPermissions(r1,Counter0)),
-    ?assertEqual(2, crdt_bcounter:localPermissions(r2,Counter0)),
-    %% Test a forbidden transference.
-    Result1 = rpc:call(FirstNode, antidote, append,
-                       [Key, Type, {{transfer, 3, r1}, r2}]),
-    ?assertEqual({error, no_permissions}, Result1),
-    %% Test transfered permissions enable the previous operation.
-    Result2 = rpc:call(FirstNode, antidote, clocksi_execute_tx,
-                       [[{update, {Key, Type, {{transfer, 2, r2}, r1}}},
-                         {update, {Key, Type, {{transfer, 3, r1}, r2}}}, {read, {Key, Type}}]]),
-    ?assertMatch({ok, _}, Result2),
-    {ok, {_, [Counter1], _}} = Result2,
-    ?assertEqual(9, crdt_bcounter:permissions(Counter1)),
-    ?assertEqual(8, crdt_bcounter:localPermissions(r1,Counter1)),
-    ?assertEqual(1, crdt_bcounter:localPermissions(r2,Counter1)).
+                       [[{update, {Key, Type, {{decrement, 8}, b}}}, {read, {Key, Type}}]]),
+    ?assertMatch({error, {no_permissions, {_, 8}}}, Result1).
 
 %% Tests the conditional write mechanism required for `generate_downstream()' and `update()' to be atomic.
 %% Such atomic execution is required for the correctness of `bcounter()' CRDT.
@@ -135,21 +105,21 @@ conditional_write_test(Nodes) ->
     Type = crdt_bcounter,
     Key = bcounter5,
     rpc:call(FirstNode, antidote, append,
-             [Key, Type, {{increment, 10}, r1}]),
+             [Key, Type, {{increment, 10}, a}]),
     %% Start a transaction on the first node and perform a read operation.
     {ok, TxId1} = rpc:call(FirstNode, antidote, clocksi_istart_tx, []),
     rpc:call(FirstNode, antidote, clocksi_iread, [TxId1, Key, Type]),
     %% Execute a transaction on the last node which performs a write operation.
     {ok, TxId2} = rpc:call(LastNode, antidote, clocksi_istart_tx, []),
     rpc:call(LastNode, antidote, clocksi_iupdate,
-             [TxId2, Key, Type, {{decrement, 3}, r1}]),
+             [TxId2, Key, Type, {{decrement, 3}, a}]),
     CommitTime1=rpc:call(LastNode, antidote, clocksi_iprepare, [TxId2]),
     ?assertMatch({ok, _}, CommitTime1),
     End1=rpc:call(LastNode, antidote, clocksi_icommit, [TxId2]),
     ?assertMatch({ok, _}, End1),
     %% Resume the first transaction and check that it fails.
     Result0 = rpc:call(FirstNode, antidote, clocksi_iupdate,
-                       [TxId1, Key, Type, {{decrement, 3}, r1}]),
+                       [TxId1, Key, Type, {{decrement, 3}, a}]),
     ?assertEqual(ok, Result0),
     CommitTime2=rpc:call(FirstNode, antidote, clocksi_iprepare, [TxId1]),
     ?assertEqual({aborted, TxId1}, CommitTime2),
