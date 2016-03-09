4d3f1f30bd2e6d25f9418854e993eb7e8d7e5e6b
Maryam Dabaghchian <mdabaghchian14@ku.edu.tr>
2016-03-10 01:40:45 +0200

    update online_store test

---
+++ riak_test/online_store.erl
@@ -0,0 +1,30 @@
+-module(online_store).
+
+%% API
+-export([add_to_store/3, remove_from_store/3]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+remove_from_store(Node, Store, Num) ->
+  lager:info("Txn1 is starting on Node: ~p", [Node]),
+  {ok, Tx1} = rpc:call(Node, antidote, start_transaction, [ignore, []]),
+  lager:info("Txn1 with ID: ~p, started on Node: ~p", [Tx1, Node]),
+  ok = rpc:call(Node, antidote, update_objects, [[{Store, decrement, Num}], Tx1]),
+  lager:info("Txn1 with ID: ~p, updated Store on Node: ~p", [Tx1, Node]),
+  {ok, [Res1]} = rpc:call(Node, antidote, read_objects, [[Store], Tx1]),
+  lager:info("Txn1 read Store val: ~b", [Res1]),
+  {ok, CT1} = rpc:call(Node, antidote, commit_transaction, [Tx1]),
+  lager:info("Txn1 with ID: ~p committed on Node: ~p", [Tx1, Node]),
+  {Res1, {Tx1, CT1}}.
+
+add_to_store(Node, Store, Num) ->
+  lager:info("Txn2 is starting on Node: ~p", [Node]),
+  {ok, Tx2} = rpc:call(Node, antidote, start_transaction, [ignore, []]),
+  lager:info("Txn2 with ID: ~p, started on Node: ~p", [Tx2, Node]),
+  ok = rpc:call(Node, antidote, update_objects, [[{Store, increment, Num}], Tx2]),
+  lager:info("Txn2 with ID: ~p, updated Store on Node: ~p", [Tx2, Node]),
+  {ok, [Res2]} = rpc:call(Node, antidote, read_objects, [[Store], Tx2]),
+  lager:info("Txn2 read Store val: ~b", [Res2]),
+  {ok, CT2} = rpc:call(Node, antidote, commit_transaction, [Tx2]),
+  lager:info("Txn2 with ID: ~p committed on Node: ~p", [Tx2, Node]),
+  {Res2, {Tx2, CT2}}.
